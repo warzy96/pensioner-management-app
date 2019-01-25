c33f9b6012c056a0895401fc656729876933a888
Ivan Varga <ivan_v@windowslive.com>
2019-01-25 20:44:07 +0100

    Changed mapping

--- DataAccessLayer/Mapping/PensionerMap.cs
+++ DataAccessLayer/Mapping/PensionerMap.cs
@@ -14,8 +14,8 @@ namespace DataAccessLayer.Mapping
             Map(x => x.PlaceOfBirth);
             Map(x => x.Surname).Not.Nullable();
             Map(x => x.MembershipStart).Not.Nullable();
-            HasMany(x => x.Payments).Cascade.DeleteOrphan();
-            HasMany(x => x.RequiredPayments).Cascade.DeleteOrphan();
+            HasMany(x => x.Payments).Cascade.AllDeleteOrphan();
+            HasMany(x => x.RequiredPayments).Cascade.AllDeleteOrphan();
             Component(x => x.CurrentAddress);
         }
     }

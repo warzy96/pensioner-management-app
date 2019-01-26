2a511f7722ccab5b0aa09f3a56a48291869eb67f
Ivan Varga <ivan_v@windowslive.com>
2019-01-26 18:23:42 +0100

    Todo: write tests. create docs

---
+++ PensionerAssociationTests/PensionerRepositoryTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace PensionerAssociationTests
+{
+    public class PensionerRepositoryTests
+    {
+        private SqliteDatabaseForTesting database;
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ PensionerAssociationTests/SqliteDatabaseForTesting.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccessLayer.Mapping;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+
+namespace PensionerAssociationTests
+{
+    public class SqliteDatabaseForTesting : IDisposable
+    {
+        protected Configuration config;
+        protected ISessionFactory sessionFactory;
+        public SqliteDatabaseForTesting()
+        {
+            var fluentConfig = Fluently.Configure()
+                    .Database(SQLiteConfiguration.Standard.UsingFile("TestDB.db")
+                    .ShowSql().FormatSql())
+                    .Mappings(mapper =>
+                    {
+                        mapper.FluentMappings.AddFromAssemblyOf<PensionerMap>();
+                    });
+
+            var nhConfiguration = fluentConfig.BuildConfiguration();
+            sessionFactory = nhConfiguration.BuildSessionFactory();
+
+            Session = sessionFactory.OpenSession();
+
+            using (var tx = Session.BeginTransaction())
+            {
+                new SchemaExport(nhConfiguration).Execute(useStdOut: true,
+                                                            execute: true,
+                                                            justDrop: false,
+                                                            connection: Session.Connection,
+                                                            exportOutput: Console.Out);
+                tx.Commit();
+            }
+
+        }
+
+        public ISession Session { get; set; }
+        public void Dispose()
+        {
+            Session.Dispose();
+            sessionFactory.Dispose();
+        }
+    }
+}
